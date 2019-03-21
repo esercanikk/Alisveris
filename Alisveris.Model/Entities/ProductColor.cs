47ce239ec1a3b22e99a927f45693c88c76ff285d
Ediz Ayyıldız <ayyildizediz@gmail.com>
2019-03-21 10:05:48 +0300

    ProductCategory ve ProductColor eklendi

---
+++ Alisveris.Model/Entities/ProductColor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alisveris.Model.Entities
+{
+
+    public class ProductColor : BaseEntity
+    {
+        public ProductColor()
+        {
+
+        }
+        public string ProductId { get; set; }
+        public Product Product { get; set; }
+        public string ColorId { get; set; }
+        public Color Color { get; set; }
+    }
+}
