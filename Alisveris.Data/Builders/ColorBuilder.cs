cc1ad4c392844cf0aa2926efa1acd41756ea2b6a
Büşra Torun <mhbusratorun@gmail.com>
2019-03-21 13:02:28 +0300

    ColorBuilder

---
+++ Alisveris.Data/Builders/ColorBuilder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Alisveris.Model.Entities;
+
+
+namespace Alisveris.Data.Builders
+{
+    public class ColorBuilder
+    {
+        public ColorBuilder(EntityTypeBuilder<Color> builder)
+        {
+            builder.HasKey(b => b.Id);
+            builder.Property(b => b.Name).IsRequired().HasMaxLength(200);
+            builder.Property(b => b.Value).IsRequired().HasMaxLength(200);
+           
+            builder.HasQueryFilter(b => !b.IsDeleted);
+        }
+
+    }
+}
+
