e59cb0747cc4f9dca27a4c5a2a792575f8ac1223
Ediz Ayyıldız <ayyildizediz@gmail.com>
2019-03-21 11:49:43 +0300

    ProductQuestion Model fixed, ProductQuestionBuilder added.

---
+++ Alisveris.Data/Builders/ProductQuestionBuilder.cs
@@ -0,0 +1,22 @@
+﻿using Alisveris.Model.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alisveris.Data.Builders
+{
+     public class ProductQuestionBuilder
+    {
+        public ProductQuestionBuilder(EntityTypeBuilder<ProductQuestion> builder)
+        {
+            builder.HasKey(b => b.Id);
+            builder.Property(b => b.Title).IsRequired().HasMaxLength(200);
+            builder.Property(b => b.Description).IsRequired();
+            builder.HasOne(b => b.Product).WithMany(c => c.ProductQuestions).HasForeignKey(p => p.ProductId);
+            builder.HasOne(b => b.QuestionCategory).WithMany(c => c.ProductQuestions).HasForeignKey(p => p.QuestionCategoryId);
+            builder.HasOne(b => b.Store).WithMany(c => c.ProductQuestions).HasForeignKey(p => p.StoreId);
+            builder.HasQueryFilter(b => !b.IsDeleted);
+        }
+    }
+}
