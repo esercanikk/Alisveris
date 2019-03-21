3a670d9772e35d60ba02bc2e51a61c9b350abcf0
smeyyekeskn <smeyyekeskn@gmail.com>
2019-03-21 11:34:34 +0300

    Shipper,Slide,Slider,Review Builder hazırlandı

---
+++ Alisveris.Data/Builders/ReviewBuilder.cs
@@ -0,0 +1,23 @@
+﻿using Alisveris.Model.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alisveris.Data.Builders
+{
+
+    public class ReviewBuilder
+    {
+        public ReviewBuilder(EntityTypeBuilder<Review> builder)
+        {
+            builder.HasKey(b => b.Id);
+            builder.Property(b => b.Name).IsRequired().HasMaxLength(200);
+            builder.Property(b => b.Email).IsRequired().HasMaxLength(200);
+            builder.HasOne(b => b.Product).WithMany(c => c.Reviews).HasForeignKey(p => p.ProductId);
+
+            builder.HasQueryFilter(b => !b.IsDeleted);
+        }
+
+    }
+}
