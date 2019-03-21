61000230519035f067682b7f93bf69132260a5c7
Murat Demirci <mdemirci@outlook.com>
2019-03-21 09:38:08 +0300

    .NET Core e-ticaret projesine başlıyoruz

---
+++ Alisveris.Model/Entities/BaseEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Alisveris.Model.Entities
+{
+    public class BaseEntity
+    {
+        public string Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public string UpdatedBy { get; set; }
+        public string IpAddress { get; set; }
+        public string Culture { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime? DeletedAt { get; set; }
+        public string DeletedBy { get; set; }
+    }
+}
