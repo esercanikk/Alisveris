08ca689a54b1a03b1dc9601703aba6870ef558d7
Ediz Ayyıldız <ayyildizediz@gmail.com>
2019-03-21 11:55:53 +0300

    ProductPhotoBuilder added.

---
+++ Alisveris.Data/Builders/ProductPhotoBuilder.cs
@@ -0,0 +1,23 @@
+﻿using Alisveris.Model.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alisveris.Data.Builders
+{
+    public class ProductPhotoBuilder
+    {
+        public ProductPhotoBuilder(EntityTypeBuilder<ProductPhoto> builder)
+        {
+            builder.HasKey(b => b.Id);
+            builder.Property(b => b.Medium).IsRequired().HasMaxLength(200);
+            builder.Property(b => b.Small).IsRequired().HasMaxLength(200);
+            builder.Property(b => b.Large).IsRequired().HasMaxLength(200);
+            builder.Property(b => b.Alt).IsRequired().HasMaxLength(4000);
+
+            builder.HasOne(b => b.Product).WithMany(c => c.Images).HasForeignKey(p => p.ProductId);
+            builder.HasQueryFilter(b => !b.IsDeleted);
+        }
+    }
+}
