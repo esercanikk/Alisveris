a4befdc99237aa2b9708d81f3116857f0c2025f1
kutlu <kutlugungor58@hotmail.com>
2019-03-21 10:02:42 +0300

    Store ve StoreBrand Entity'si tamamlandı

---
+++ Alisveris.Model/Entities/Store.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alisveris.Model.Entities
+{
+    public class Store:BaseEntity
+    {
+        public string Name { get; set; }
+        public string Slug { get; set; }
+        public string Owner { get; set; }
+        public string Logo { get; set; }
+        public string ContactName { get; set; }
+        public string ContactPhone { get; set; }
+        public string ContactEmail { get; set; }
+        public string Address { get; set; }
+        public string Description { get; set; }
+        public virtual ICollection<Product> Products { get; set; }
+        public virtual ICollection<StoreBrand> StoreBrands { get; set; }
+        public virtual ICollection<Coupon> Coupons { get; set; }
+        public virtual ICollection<ProductQuestion> ProductQuestions { get; set; }
+    }
+}
---
+++ Alisveris.Model/Entities/StoreBrand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alisveris.Model.Entities
+{
+    public class StoreBrand:BaseEntity
+    {
+        public string StoreId { get; set; }
+        public Store Store { get; set; }
+
+        public string BrandId { get; set; }
+        public Brand Brand { get; set; }
+    }
+}
