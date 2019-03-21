ce7fc9865afe31f7d86f044b6e2bd4d1617b1e51
alifurkangokce <alifurkangokce@gmail.com>
2019-03-21 11:23:29 +0300

    ali

---
+++ Alisveris.Data/Builders/PostBuilder.cs
@@ -0,0 +1,21 @@
+﻿using Alisveris.Model.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alisveris.Data.Builders
+{
+    public class PostBuilder
+    {
+        public PostBuilder(EntityTypeBuilder<Post> builder)
+        {
+            builder.HasKey(b => b.Id);
+            builder.Property(b => b.Title).IsRequired().HasMaxLength(200);
+            builder.Property(b => b.Slug).IsRequired().HasMaxLength(200);
+            builder.Property(b => b.MetaTitle).HasMaxLength(200);
+
+            builder.HasQueryFilter(b => !b.IsDeleted);
+        }
+    }
+}
