91d642c40a7b6ea8d73d6bd486b88244502dce39
Murat Demirci <mdemirci@outlook.com>
2019-03-22 12:27:30 +0300

    generic servis katmanı oluşturuluyor

---
+++ Alisveris.Service/CommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alisveris.Service
+{
+    public interface ICommandHandler
+    {
+        dynamic Handle(Command command);
+    }
+
+    public interface ICommandHandler<T> : ICommandHandler where T : Command
+    {
+        dynamic Handle(T command);
+    }
+
+
+    public abstract class CommandHandler<T> : ICommandHandler<T> where T : Command
+    {
+        public abstract dynamic Handle(T command);
+
+        public virtual Task<dynamic> HandleAsync(T command)
+        {
+            throw new Exception("not implemented");
+        }
+
+
+        public dynamic Handle(Command command)
+        {
+            return Handle((T)command);
+        }
+    }
+}
---
+++ Alisveris.Service/Result.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Alisveris.Service
+{
+    public class Result
+    {
+        public Result()
+        {
+
+        }
+        public Result(string message)
+        {
+            Message = message;
+        }
+        public Result(string message, bool hasError)
+        {
+            Message = message;
+            HasError = hasError;
+        }
+        public Result(dynamic value, bool tokenStatus, string message)
+        {
+            Message = message;
+            Value = value;
+            TokenStatus = tokenStatus;
+        }
+        public Result(dynamic value, bool tokenStatus, string message, bool hasError, string stackTrace)
+        {
+            Message = message;
+            Value = value;
+            TokenStatus = tokenStatus;
+            HasError = hasError;
+            StackTrace = stackTrace;
+        }
+        public Result(dynamic value, bool tokenStatus, string message, bool hasError)
+        {
+            Message = message;
+            Value = value;
+            TokenStatus = tokenStatus;
+            HasError = hasError;
+        }
+        public Result(dynamic value, bool tokenStatus, string message, bool hasError, long totalRecordCount)
+        {
+            Message = message;
+            Value = value;
+            TokenStatus = tokenStatus;
+            HasError = hasError;
+            TotalRecordCount = totalRecordCount;
+        }
+        public Result(dynamic value, long totalRecordCount, string message)
+        {
+            Message = message;
+            Value = value;
+            TotalRecordCount = totalRecordCount;
+        }
+
+        public dynamic Value { get; set; }
+        public bool HasError { get; set; }
+        public string Message { get; set; }
+        public long TotalRecordCount { get; set; }
+        public bool TokenStatus { get; set; }
+        public string StackTrace { get; set; }
+    }
+}
