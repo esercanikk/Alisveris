d36edcb6dd9c7c00bf67358b8d435e6649b3d907
kutlu <kutlugungor58@hotmail.com>
2019-03-25 12:57:33 +0300

    Kutlu

---
+++ Alisveris.Service/Queries/Commerce/CouponQuery.cs
@@ -0,0 +1,25 @@
+﻿using Alisveris.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alisveris.Service.Queries
+{
+    public class CouponQuery:Query
+    {
+        public string Id { get; set; }
+        public string CouponNo { get; set; }
+        public int CouponNumber { get; set; }
+        public string Name { get; set; }
+        public string OrderId { get; set; }
+        public Order Order { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string StoreId { get; set; }
+        public Alisveris.Model.Entities.Store Store { get; set; }
+        public decimal MinTotalPrice { get; set; }
+        public decimal Discount { get; set; }
+        public string Conditions { get; set; }
+
+    }
+}
