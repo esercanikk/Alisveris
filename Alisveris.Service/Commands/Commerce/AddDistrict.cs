765505d8b16cb8f1ac7f2f4e9dafd8719aa647a9
Ediz Ayyıldız <ayyildizediz@gmail.com>
2019-03-25 13:49:17 +0300

    Bazı commandlar ve Handlerlar eklendi.

---
+++ Alisveris.Service/Commands/Commerce/AddDistrict.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alisveris.Service.Commands
+{
+    [Describe(CommandType.Commerce, Authorities.Create, "Yeni ilçe oluşturur.")]
+    public class AddDistrict : Command
+    {
+        public string Name { get; set; }
+        public string CityId { get; set; }
+    }
+}
