869c0ca5af93e745ddcce3998b5040a991a30a25
Yigitcan Balcin <ybalcin1@gmail.com>
2019-03-25 11:47:32 +0300

    SearchBrands,SearchCoupons,SearchProductCategories ve SystemCommands yapıldı

---
+++ Alisveris.Service/Commands/System/GetModules.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alisveris.Service.Commands
+{
+    [Describe(CommandType.System, Authorities.Read, "Modül Listesi")]
+    public class GetModules : Command
+    {
+    }
+}
---
+++ Alisveris.Service/Queries/Commerce/ProductCategoryQuery.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alisveris.Service.Queries.Commerce
+{
+    public class ProductCategoryQuery
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Slug { get; set; }
+        public string Description { get; set; }
+        public string Photo { get; set; }
+        public string ParentId { get; set; }
+        public bool HasSubCategory { get; set; }
+
+    }
+}
