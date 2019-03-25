3770b9c1054b568cffd7293c6c0cab0b6b1566d8
smeyyekeskn <smeyyekeskn@gmail.com>
2019-03-25 12:57:32 +0300

    sümeyye

---
+++ Alisveris.Service/Commands/Commerce/SearchShippers.cs
@@ -0,0 +1,30 @@
+﻿using Alisveris.Model.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alisveris.Service.Commands
+{
+    [Describe(CommandType.Commerce, Authorities.Read, "Kargo firması arar.")]
+    public class SearchShippers : Command, ISearchCommand
+    {
+        public SearchShippers()
+        {
+            IsAdvancedSearch = false;
+            SortField = "createdAt";
+            SortOrder = "desc";
+            IsPagedSearch = false;
+            PageNumber = 1;
+            PageSize = 10;
+        }
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public bool IsAdvancedSearch { get; set; }
+        public string SortOrder { get; set; }
+        public string SortField { get; set; }
+        public bool IsPagedSearch { get; set; }
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public Condition? Condition { get; set; }
+    }
+}
