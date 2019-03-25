8fe382fab5a57951b9f084cb53e263b6ddf2a0b3
sena <sena@sena>
2019-03-25 12:35:35 +0200

    productcolor

---
+++ Alisveris.Service/Handlers/Commerce/AddProductColorHandler.cs
@@ -0,0 +1,51 @@
+﻿using Alisveris.Data;
+using Alisveris.Model.Entities;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alisveris.Service.Handlers
+{
+    public class AddProductColorHandler : CommandHandler<Commands.AddProductColor>
+    {
+        private readonly IRepository<ProductColor> productcolorRepository;
+        private readonly IUnitOfWork unitOfWork;
+        public AddProductColorHandler(IUnitOfWork unitOfWork, IRepository<ProductColor> productcolorRepository)
+        {
+            this.unitOfWork = unitOfWork;
+            this.productcolorRepository = productcolorRepository;
+        }
+        public override async Task<dynamic> HandleAsync(Commands.AddProductColor command)
+        {
+            Result result;
+            // validate the command
+            if (string.IsNullOrWhiteSpace(command.ProductId))
+            {
+               
+                result = new Result(false, command.ProductId, "Ürün Id gereklidir.", true, null);
+                return await Task.FromResult(result);
+            }
+            if (string.IsNullOrWhiteSpace(command.ColorId))
+            {                
+
+                result = new Result(false, command.ColorId, "Ürün Rengi Id gereklidir.", true, null);
+                return await Task.FromResult(result);
+            }
+
+            // map command to the model
+            var model = Mapper.Map<ProductColor>(command);
+
+            // mark the model to insert
+            productcolorRepository.Insert(model);
+
+            // save changes to database
+            await unitOfWork.SaveChangesAsync();           
+
+            result = new Result(true, model.Id, "Ürün rengi başarıyla eklendi.", true, 1);
+            // return the result
+            return await Task.FromResult(result);
+        }
+    }
+}
