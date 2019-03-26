d70a0f1a5b201e6638acd4d9a0e9e6494e55d020
Ediz Ayyıldız <ayyildizediz@gmail.com>
2019-03-26 11:34:25 +0300

    EditProductQuestion added, EditProductQuestion added.

---
+++ Alisveris.Service/Commands/Commerce/EditQuestionCategory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alisveris.Service.Commands
+{
+    [Describe(CommandType.Commerce, Authorities.Update, "Soru kategorisi günceller.")]
+    public class EditQuestionCategory : Command
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Alisveris.Service/Handlers/Commerce/EditQuestionCategoryHandler.cs
@@ -0,0 +1,55 @@
+﻿using Alisveris.Data;
+using Alisveris.Model.Entities;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alisveris.Service.Handlers
+{
+    public class EditQuestionCategoryHandler : CommandHandler<Commands.EditQuestionCategory>
+    {
+        private readonly IRepository<QuestionCategory> questionCategoryRepository;
+        private readonly IUnitOfWork unitOfWork;
+        public EditQuestionCategoryHandler(IUnitOfWork unitOfWork, IRepository<QuestionCategory> questionCategoryRepository)
+        {
+            this.unitOfWork = unitOfWork;
+            this.questionCategoryRepository = questionCategoryRepository;
+        }
+        public override async Task<dynamic> HandleAsync(Commands.EditQuestionCategory command)
+        {
+            Result result;
+            // validate the command
+            if (string.IsNullOrWhiteSpace(command.Id))
+            {
+                result = new Result(true, command.Id, "Id gereklidir.", true, null);
+                return await Task.FromResult(result);
+            }
+            if (string.IsNullOrWhiteSpace(command.Name))
+            {
+                result = new Result(true, command.Name, "Soru kategorisi gereklidir.", true, null);
+                return await Task.FromResult(result);
+            }
+            if (command.Name.Length > 200)
+            {
+                result = new Result(true, command.Name, "Soru kategorisi 200 karakterden uzun olamaz.", true, null);
+                return await Task.FromResult(result);
+            }
+
+
+            // map command to the model
+            var model = Mapper.Map<QuestionCategory>(command);
+
+            // mark the model to update
+            questionCategoryRepository.Update(model);
+
+            // save changes to database
+            unitOfWork.SaveChanges();
+
+            // return the result
+            result = new Result(true, model.Id, "Soru kategorisi başarıyla güncellendi.", false, 1);
+            return await Task.FromResult(result);
+        }
+    }
+}
