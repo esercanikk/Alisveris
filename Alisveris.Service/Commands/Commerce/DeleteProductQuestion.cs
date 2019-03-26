c1061b2516beaba12f7732bcb8e89d2140a77f6c
Ayşegül Yılmaz <ayseguulyilmaz@gmail.com>
2019-03-26 10:50:27 +0300

    Handler işleri

---
+++ Alisveris.Service/Commands/Commerce/DeleteProductQuestion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alisveris.Service.Commands
+{
+    [Describe(CommandType.Commerce, Authorities.Delete, "Ürün sorusu silindi.")]
+    public class DeleteProductQuestion : Command
+    {
+        public string Id { get; set; }
+
+    }
+}
