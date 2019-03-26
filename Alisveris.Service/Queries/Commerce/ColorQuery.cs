22c1cf3513c65f3fc63ddbb24a94269d0a5b5c94
sena <sena@sena>
2019-03-26 11:04:55 +0200

    color

---
+++ Alisveris.Service/Queries/Commerce/ColorQuery.cs
@@ -0,0 +1,16 @@
+﻿using Alisveris.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alisveris.Service.Queries.Commerce
+{
+    public class ColorQuery : Query
+    {
+       
+        public string Name { get; set; }
+        public string Value { get; set; }
+
+
+    }
+}
