f5eb26484a7260b55de9fb28603700560d4fecd0
Yigitcan Balcin <ybalcin1@gmail.com>
2019-03-26 10:57:41 +0300

    GetAdvertisement,GetPostCategory,GetPostPostCategory,GetStoreBrand,GetDistrict ve GetQuestionCategory yapıldı

---
+++ Alisveris.Service/Handlers/Cms/GetPostPostCategoryHandler.cs
@@ -0,0 +1,40 @@
+﻿using Alisveris.Data;
+using Alisveris.Model.Entities;
+using Alisveris.Service.Queries;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alisveris.Service.Handlers.Cms
+{
+    public class GetPostPostCategoryHandler : CommandHandler<Commands.GetPostPostCategory>
+    {
+        private readonly IRepository<PostPostCategory> postpostcategoryRepository;
+        public GetPostPostCategoryHandler(IRepository<PostPostCategory> postpostcategoryRepository)
+        {
+            this.postpostcategoryRepository = postpostcategoryRepository;
+        }
+        public override async Task<dynamic> HandleAsync(Commands.GetPostPostCategory command)
+        {
+            Result result;
+            // get the model from database
+            var model = postpostcategoryRepository.Get(command.Id);
+
+            // if nothing found
+            if (model == null)
+            {
+                result = new Result(true, command.Id, "Yazı ile yazı kategorisi arasındaki ilişki bulunamadı.", true, 0);
+                // return the not found result
+                return await Task.FromResult(result);
+            }
+            // map the model to query
+            var value = Mapper.Map<PostPostCategoryQuery>(model);
+
+            // return the query result
+            result = new Result(true, value, "1 adet yazı ile yazı kategorisi arasındaki ilişki bulundu.", false, 1);
+            return await Task.FromResult(result);
+        }
+    }
+}
---
+++ Alisveris.Service/Handlers/Commerce/GetDistrictHandler.cs
@@ -0,0 +1,40 @@
+﻿using Alisveris.Data;
+using Alisveris.Model.Entities;
+using Alisveris.Service.Queries;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alisveris.Service.Handlers.Commerce
+{
+    public class GetDistrictHandler : CommandHandler<Commands.GetDistrict>
+    {
+        private readonly IRepository<District> districtRepository;
+        public GetDistrictHandler(IRepository<District> districtRepository)
+        {
+            this.districtRepository = districtRepository;
+        }
+        public override async Task<dynamic> HandleAsync(Commands.GetDistrict command)
+        {
+            Result result;
+            // get the model from database
+            var model = districtRepository.Get(command.Id);
+
+            // if nothing found
+            if (model == null)
+            {
+                result = new Result(true, command.Id, "İlçe bulunamadı.", true, 0);
+                // return the not found result
+                return await Task.FromResult(result);
+            }
+            // map the model to query
+            var value = Mapper.Map<DistrictQuery>(model);
+
+            // return the query result
+            result = new Result(true, value, "1 adet ilçe bulundu.", false, 1);
+            return await Task.FromResult(result);
+        }
+    }
+}
