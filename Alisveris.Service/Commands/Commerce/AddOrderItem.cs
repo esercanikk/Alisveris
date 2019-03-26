51cf42cd2971878cecab952b220e2ef98f070c6b
alifurkangokce <alifurkangokce@gmail.com>
2019-03-26 10:40:31 +0300

    ali

---
+++ Alisveris.Service/Commands/Commerce/AddOrderItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alisveris.Service.Commands
+{
+    [Describe(CommandType.Commerce, Authorities.Create, "Yeni sipariş öğesi oluşturur.")]
+    public class AddOrderItem : Command
+    {
+        public string OrderId { get; set; }
+        public string ProductId { get; set; }
+        public string ShipperId { get; set; }
+        public int Quantity { get; set; }
+
+
+
+
+    }
+}
